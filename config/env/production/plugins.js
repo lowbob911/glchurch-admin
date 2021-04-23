c88930e2f3b69f40ec6a1112d1f562af1bd1c0e5
Sergey Sherby <sergey.sherby@sportsfusion.co.uk>
2021-04-23 23:57:11 +0300

    s3 creds

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: 'AKIAWOSZWSMNWDN6AXUP',
-      secretAccessKey: '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',
+      accessKeyId: 'AKIAWOSZWSMNYPJZLAJI',
+      secretAccessKey: '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',
       region: 'eu-north-1',
       params: {
         Bucket: 'strapi-admin-bucket',
